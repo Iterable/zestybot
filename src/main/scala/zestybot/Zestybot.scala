d232b9805bf1418986486edf527bd73f116b4b4d
Thomas Kim <tk@iterable.com>
2018-04-09 16:38:25 -0700

    Post to slack

--- src/main/scala/zestybot/Zestybot.scala
+++ src/main/scala/zestybot/Zestybot.scala
@@ -1,5 +1,7 @@
 package zestybot
 
+import java.net.URI
+
 import com.softwaremill.sttp._
 import com.softwaremill.sttp.akkahttp.AkkaHttpBackend
 import com.softwaremill.sttp.circe._
@@ -7,9 +9,21 @@ import com.softwaremill.sttp.circe._
 import scala.concurrent.{Await, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
 
-class Zestybot(clientId: String) {
+case class SlackMessage(text: String, attachments: Seq[Attachment])
+case class Attachment(title: String, text: String, image_url: String)
+
+class Zestybot(clientId: String, slackWebhookUri: String) {
   implicit val backend = AkkaHttpBackend()
   import io.circe.generic.auto._, io.circe.syntax._
+
+  def doIt() = {
+    slackableMessage.flatMap(postToSlackWebhook)
+    backend.close()
+  }
+
+  def postToSlackWebhook(slackMessage: SlackMessage) = {
+    sttp.post(Uri(new URI(slackWebhookUri))).body(slackMessage).send()
+  }
 
   def slackableMessage = {
     for {
@@ -20,10 +34,7 @@ class Zestybot(clientId: String) {
         sttp.get(uri"https://api.zesty.com/portal_api/dishes/${mealItem.dish_id}").response(asJson[Dishes]).send().map(_.unsafeBody.right.get.dishes.head)
       }
     } yield {
-      s"""
-         |${todaysMeal.restaurant_name}
-         |${dishes.map(_.description).mkString(",")}
-       """.stripMargin
+      SlackMessage(todaysMeal.restaurant_name, dishes.map { dish => Attachment(dish.name, dish.description, dish.full_image_path) })
     }
   }
 }
@@ -31,6 +42,7 @@ class Zestybot(clientId: String) {
 object Zestybot {
   def main(args: Array[String]) = {
     import scala.concurrent.duration._
-    println(Await.result(new Zestybot(args(0)).slackableMessage, 10.seconds))
+    val zestybot = new Zestybot(args(0), args(1))
+    zestybot.doIt()
   }
 }
